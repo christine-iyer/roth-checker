1161fd847ce53f987c73df555f8764a9b1a2796b
chrisIyer <hellohotbox420@gmail.com>
2023-02-24 16:13:02 -0500

    added server.

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router = require('express').Router()
+const app = express()
+const fetch = require('node-fetch')
+app.get('/', function(req,res){
+     res.send(`<h1>Haitch 1</h1>`)
+})
+app.listen(3008, function(){
+     console.log('listening on 3008')
+})
