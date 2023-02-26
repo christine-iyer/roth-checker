69d8f01d470cd34159c65079aa495a377c7535f5
chrisIyer <hellohotbox420@gmail.com>
2023-02-26 16:56:59 -0500

    still unsure after troubleshooting with Jake why not working. It's workin for him. "

--- server.js
+++ server.js
@@ -16,25 +16,25 @@ app.use(express.static)
 console.log('before')
 app.use('/api/portfolio', require('./routes/api/portfolio'))
 
-// router.route('/api/portfolio').get(async function(req,res) {
-//   console.log("hi")
-//      try {
-//        const response = await fetch('https://yh-finance.p.rapidapi.com/market/v2/get-quotes?region=US&symbols=GOOGL%2CAMZN%2CAAPL%2CBA%2CCOIN%2CCVS%2CGS%2CMS%2CNVDA%2CPYPL%2CPFE%2CCRM%2CSBUX%2CTSLA%2CDIS%2CVTI%2CLI', {
-//          method: "GET",
-//          headers: {
-//            'X-RapidAPI-Key': '5e4d0eeb5bmsh1f0574004d6dfb6p160e9fjsnd9a3ae03ad63',
-//            'X-RapidAPI-Host': 'yh-finance.p.rapidapi.com'
-//          }
-//        })
-//        const data = await response.json()
-//        //res.locals.data.quoteResponse.results = data.quoteResponse.results
-//        console.log(data.quoteResponse)
-//      } catch (e) {
-//        console.error(e);
-//        return e.message
-//      }
-//      res.json(data.quoteResponse)
-//    })
+router.route('/api/portfolio').get(async function(req,res) {
+  console.log("hi")
+     try {
+       const response = await fetch('https://yh-finance.p.rapidapi.com/market/v2/get-quotes?region=US&symbols=GOOGL%2CAMZN%2CAAPL%2CBA%2CCOIN%2CCVS%2CGS%2CMS%2CNVDA%2CPYPL%2CPFE%2CCRM%2CSBUX%2CTSLA%2CDIS%2CVTI%2CLI', {
+         method: "GET",
+         headers: {
+           'X-RapidAPI-Key': '5e4d0eeb5bmsh1f0574004d6dfb6p160e9fjsnd9a3ae03ad63',
+           'X-RapidAPI-Host': 'yh-finance.p.rapidapi.com'
+         }
+       })
+       const data = await response.json()
+       //res.locals.data.quoteResponse.results = data.quoteResponse.results
+       console.log(data.quoteResponse)
+     } catch (e) {
+       console.error(e);
+       return e.message
+     }
+     res.json(data.quoteResponse)
+   })
 app.get('/api/portfolio', function(req,res){
      res.send("<h1>Haitch 1</h1>")
 })
