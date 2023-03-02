2a01762e829b4e13d168f598be9e5fcc59765980
chrisIyer <hellohotbox420@gmail.com>
2023-03-01 20:11:25 -0500

    WORKING ON DATA AND VIEWS.

--- controllers/api/portfolio.js
+++ controllers/api/portfolio.js
@@ -1,26 +1,26 @@
 require('dotenv').config()
 const express = require('express')
-const router = require('express').Router()
+
 const axios = require("axios");
 
 const hardcodedData = [
-  { symbol: "GOOGL", purchasePrice: 143.49, shares: 100, principalDate: '12/22/2021' },
-  { symbol: "TSLA", purchasePrice: 242.54, shares: 45, principalDate: '11/27/2020' },
-  { symbol: "AMZN", purchasePrice: 160.00, shares: 80, principalDate: '11/27/2020' },
-  { symbol: "BA", purchasePrice: 188.59, shares: 25, principalDate: '12/14/2022' },
-  { symbol: "COIN", purchasePrice: 257.31, shares: 50, principalDate: '12/28/2021' },
-  { symbol: "AAPL", purchasePrice: 114.56, shares: 200, principalDate: ' 11/23/2020' },
-  { symbol: "CVS", purchasePrice: 89.83, shares: 25, principalDate: '1/11/2023' },
-  { symbol: "GS", purchasePrice: 342.94, shares: 10, principalDate: '1/24/2022' },
-  { symbol: "MS", purchasePrice: 98.39, shares: 25, principalDate: '12/22/2021' },
-  { symbol: "NVDA", purchasePrice: 293.75, shares: 5, principalDate: '12/22/2021' },
-  { symbol: "PYPL", purchasePrice: 191.57, shares: 10, principalDate: '12/22/2021' },
-  { symbol: "PFE", purchasePrice: 47.45, shares: 100, principalDate: '11/10/2022' },
-  { symbol: "CRM", purchasePrice: 160.38, shares: 20, principalDate: '10/21/2022' },
-  { symbol: "SBUX", purchasePrice: 102.99, shares: 25, principalDate: '12/14/2022' },
-  { symbol: "DIS", purchasePrice: 151.93, shares: 10, principalDate: '12/22/2021' },
-  { symbol: "VTI", purchasePrice: 239.05, shares: 10, principalDate: '12/22/2021' },
-  { symbol: "LI", purchasePrice: 32.53, shares: 225, principalDate: '11/13/2020' }
+  { symbol: "GOOGL", purchasePrice: 143.49, shares: 100, principalDate: '12/22/2021' , exchangeName: 'NasdaqGS' },
+  { symbol: "TSLA", purchasePrice: 242.54, shares: 45, principalDate: '11/27/2020', exchangeName: 'NasdaqGS' },
+  { symbol: "AMZN", purchasePrice: 160.00, shares: 80, principalDate: '11/27/2020', exchangeName: 'NasdaqGS'  },
+  { symbol: "BA", purchasePrice: 188.59, shares: 25, principalDate: '12/14/2022', exchangeName: 'NYSE' },
+  { symbol: "COIN", purchasePrice: 257.31, shares: 50, principalDate: '12/28/2021', exchangeName: 'NasdaqGS' },
+  { symbol: "AAPL", purchasePrice: 114.56, shares: 200, principalDate: ' 11/23/2020', exchangeName: 'NasdaqGS' },
+  { symbol: "CVS", purchasePrice: 89.83, shares: 25, principalDate: '1/11/2023', exchangeName: 'NYSE' },
+  { symbol: "GS", purchasePrice: 342.94, shares: 10, principalDate: '1/24/2022', exchangeName: 'NYSE' },
+  { symbol: "MS", purchasePrice: 98.39, shares: 25, principalDate: '12/22/2021', exchangeName: 'NYSE' },
+  { symbol: "NVDA", purchasePrice: 293.75, shares: 5, principalDate: '12/22/2021', exchangeName: 'NasdaqGS' },
+  { symbol: "PYPL", purchasePrice: 191.57, shares: 10, principalDate: '12/22/2021', exchangeName: 'NasdaqGS' },
+  { symbol: "PFE", purchasePrice: 47.45, shares: 100, principalDate: '11/10/2022', exchangeName: 'NYSE' },
+  { symbol: "CRM", purchasePrice: 160.38, shares: 20, principalDate: '10/21/2022', exchangeName: 'NYSE' },
+  { symbol: "SBUX", purchasePrice: 102.99, shares: 25, principalDate: '12/14/2022', exchangeName: 'NasdaqGS' },
+  { symbol: "DIS", purchasePrice: 151.93, shares: 10, principalDate: '12/22/2021', exchangeName: 'NYSE' },
+  { symbol: "VTI", purchasePrice: 239.05, shares: 10, principalDate: '12/22/2021', exchangeName:  'NYSEArca'},
+  { symbol: "LI", purchasePrice: 32.53, shares: 225, principalDate: '11/13/2020', exchangeName: 'NasdaqGS'}
 
 ];
 function get(req,res, next){
@@ -31,7 +31,7 @@ function get(req,res, next){
     url: 'https://yh-finance.p.rapidapi.com/market/v2/get-quotes',
     params: {
       region: 'US',
-      symbols: 'GOOGL,TSLA,AMZN,BA,COIN,AAPL,CVS,GS,MS,NVDA,PYPL,PFE,CRM,SBUX,DIS,VIT,LI'
+      symbols: 'GOOGL,TSLA,AMZN,BA,COIN,AAPL,CVS,GS,MS,NVDA,PYPL,PFE,CRM,SBUX,DIS,VTI,LI'
     },
     headers: {
       'X-RapidAPI-Key': '5e4d0eeb5bmsh1f0574004d6dfb6p160e9fjsnd9a3ae03ad63',
@@ -40,23 +40,31 @@ function get(req,res, next){
   };
   
   axios.request(options).then(function (response) {
-    console.log(" Symbol: "+JSON.stringify(response.data.quoteResponse.result[1].symbol
-      + ": today it's worth "+JSON.stringify(response.data.quoteResponse.result[1].regularMarketPrice*hardcodedData[1].shares
-        + ", when I bought it I paid  "+JSON.stringify(hardcodedData[1].purchasePrice*hardcodedData[1].shares
-        ))
-      ));
-      next()
+
+
+////////////////////////////////////
+    console.log(
+      " Symbol: " + JSON.stringify(response.data.quoteResponse.result[16].symbol ) + 
+      " aka: " + JSON.stringify(response.data.quoteResponse.result[16].longName ) + 
+      " Exchange Name: " + JSON.stringify(response.data.quoteResponse.result[16].fullExchangeName ) + 
+      " Today's Price: " + JSON.stringify(response.data.quoteResponse.result[16].regularMarketPrice ) + 
+      " Today it's worth: " + JSON.stringify(response.data.quoteResponse.result[16].regularMarketPrice*hardcodedData[16].shares) + 
+      " When I bought it I paid:  " + JSON.stringify(hardcodedData[16].purchasePrice*hardcodedData[16].shares));
+/////////////////////////////////////
+
+
+next()
    }).catch(function (error) {
     console.error(error);
   });
 
 }
-async function index(req, res) {
+ function index(req, res) {
   try {
     const result = response => {
-      const mergedData = [...hardcodedData, ...response.data]
+      const mergedData = [...hardcodedData, ...response]
       res.json(mergedData)
-      console.log("merged data: " + mergedData )
+      console.log("merged data: " + JSON.stringify(mergedData[3]) )
     }
     return result
   }
--- routes/api/portfolio.js
+++ routes/api/portfolio.js
@@ -2,4 +2,4 @@ const express = require('express')
 const router = express.Router()
 const portfolioController = require('../../controllers/api/portfolio')
 router.get('/', portfolioController.get, portfolioController.index)
-module.exports = router
+module.exports = router 
