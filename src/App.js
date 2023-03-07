9135836689578a6ad747ce5b3e53f04aeedc53ff
chrisIyer <hellohotbox420@gmail.com>
2023-03-07 09:20:24 -0500

    front and back starting to connect.

--- src/App.js
+++ src/App.js
@@ -2,26 +2,48 @@ import { useState, useEffect } from 'react';
 import './App.css';
 const SERVER_URL="http://localhost:3008/api/portfolio/"
 
-function App() {async function handleClick(){
-  //const [mergedData, setMergedData] = useState([])
-      await fetch(SERVER_URL)
+function App() {
+  const [mergedData, setMergedData] = useState([{
+  symbol: '',
+  longName:'',
+  fullExchangeName: '', 
+  purchasePrice: '',
+  regularMarketPrice: '',
+  shares: '' 
+}])
+  async function handleClick(){
 
+
+
+      await fetch(SERVER_URL)
       .then(res=>res.json())
-      .then(
-        (result)=> {
-          console.log(result)
-        }
-        )
+      .then((result)=> {console.log(result)})
 
       
       
     }
+    useEffect(() => {
+    setMergedData();
+    }, []);
   return (
     <div className="App">
       <header className="App-header">
         <div>
         <button type='submit' onClick={handleClick}>Click</button>
+
         </div>
+        <>
+      {mergedData && mergedData.regularMarketPrice ? (
+        <div>
+          <h1>
+            {mergedData.regularMarketPrice}/{mergedData.shares}
+          </h1>
+          <h2>{mergedData.symbol}</h2>
+        </div>
+      ) : (
+        <h1>Waiting on Server </h1>
+      )}
+    </>
        </header>
     </div>
   );
