841af108af8fa01d0f855b8309afc9e5bfc6d106
chrisIyer <hellohotbox420@gmail.com>
2023-03-09 13:01:37 -0500

    it's working!

--- src/App.js
+++ src/App.js
@@ -5,24 +5,61 @@ const SERVER_URL = "http://localhost:3008/api/portfolio/"
 
 function App() {
   const [mergedData, setMergedData] = useState([])
+  const [errorMessage, setErrorMessage] = useState("");
 
-  async function handleClick() {
-    await fetch(SERVER_URL)
-      .then(res => res.json())
-      .then((results) => {
-        console.log(results)
-        console.log(results.map(i=>("symbol:"+i.longName)))
-        setMergedData([...results])
-        return setMergedData
-      })
-  }
-useEffect(() => {setMergedData()}, [mergedData]);
+  const getMergedData = async () => {
+    try {
+      const response = await fetch(
+        SERVER_URL
+      );
+      const data = await response.json();
+      setMergedData(data);
+      console.log(data)
+    } catch (e) {
+      console.error(e);
+      setErrorMessage(e.message);
+      // callback(err,null)
+    }
+  };
+//mergedata=[{symbol, longName, fullExchangeName, principalDate,regularMarketPrice, shares} = {mergedData}] 
+  
+  // async function getData() {
+  //   await fetch(SERVER_URL)
+  //     .then(res => res.json())
+  //     .then((results) => {
+  //       console.log(results)
+  //       console.log(results?.[0])
+  //         // results.map(i=>("symbol:"+i.longName)))
+  //       setMergedData([results?.[12]])
+  //       console.log(results?.[12])
+  //     })
+  // }
+  console.log(mergedData.length)
+useEffect(() => {}, []);
   return (
     <div className="App">
       <header className="App-header">
         <div>
-          <button type='submit' onClick={handleClick}>Click to prompt the Server to get current stock prices</button>
+          <button type='submit' onClick={getMergedData}>Click to prompt the Server to get current stock prices</button>
         </div>
+        <div>
+
+    <div   className='cass'>
+    <ul className='happy'>
+      {
+            mergedData && mergedData.length
+              ? mergedData.map(data => (
+                <li
+                  key={data?.symbol}
+                  blog={data}>{data.symbol}</li>
+              ))
+              : <>
+                <h2>No Blogs Yet... Add one in the Form Above</h2>
+                </>
+        }
+    </ul>
+    </div>
+    </div>
       </header>
 
     </div>
