0b7b94b53f49bacbf011bfee15b4db3462cbfea8
chrisIyer <hellohotbox420@gmail.com>
2024-02-13 05:55:14 -0500

    trying to find when DND context worked. No luck.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
-import { React, useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { DndContext, closestCenter } from '@dnd-kit/core';
-import { arrayMove,SortableContext,
-verticalListSortingStrategy,
-} from '@dnd-kit/sortable';
-import { SortableItem } from './components/SortableItem';
-import List from './components/List';
-
-import { Card } from 'react-bootstrap';
 import './App.css';
-// import FormTextExample from './components/FormTextExample';
-// import SendEmail from './components/SendEmail'
-// import ImageFile from './components/ImageFile';
+import Container from 'react-bootstrap/Container';
+import { DndContext, closestCenter } from "@dnd-kit/core";
+import { arrayMove, SortableContext, verticalListSortingStrategy } from "@dnd-kit/sortable";
+import { SortableItem } from './components/SortableItem'
+
+
 
 import './App.css';
 const SERVER_URL = "http://localhost:3008/api/portfolio/"
@@ -19,14 +14,6 @@ const SERVER_URL = "http://localhost:3008/api/portfolio/"
 function App() {
   const [mergedData, setMergedData] = useState([])
   const [errorMessage, setErrorMessage] = useState("");
-  // const [images, setImages] = useState([]);
-  // const maxNumber = 69;
-
-  // const onChange = (imageList, addUpdateIndex) => {
-  //   // data for submit
-  //   console.log(imageList, addUpdateIndex);
-  //   setImages(imageList);
-  //   };
 
   const getMergedData = async () => {
     try {
@@ -39,11 +26,24 @@ function App() {
     } catch (e) {
       console.error(e);
       setErrorMessage(e.message);
+      // callback(err,null)
     }
   };
-
+//mergedata=[{symbol, longName, fullExchangeName, principalDate,regularMarketPrice, shares} = {mergedData}] 
+  
+  // async function getData() {
+  //   await fetch(SERVER_URL)
+  //     .then(res => res.json())
+  //     .then((results) => {
+  //       console.log(results)
+  //       console.log(results?.[0])
+  //         // results.map(i=>("symbol:"+i.longName)))
+  //       setMergedData([results?.[12]])
+  //       console.log(results?.[12])
+  //     })
+  // }
   console.log(mergedData.length)
-  useEffect(() => { }, []);
+useEffect(() => {}, []);
   return (
     <div className="App">
       <header className="App-header">
@@ -51,41 +51,164 @@ function App() {
           <button type='submit' onClick={getMergedData}>Click to prompt the Server to get current stock prices</button>
         </div>
         <div>
-          <div className='cass'>
-            <ul className='happy'>
-              {
-                mergedData && mergedData.length
-                  ? mergedData.map(data => (
-                    <Card
-                      key={data?.symbol}
-                      blog={data}>
-                        {data.symbol + ' $' + (data.regularMarketPrice * data.shares).toLocaleString() +
-                        ', you paid $' +
-                        (data.purchasePrice * data.shares).toLocaleString() +
-                        ' a  ' + (((data.regularMarketPrice * data.shares) - (data.purchasePrice * data.shares)) / (data.purchasePrice * data.shares) * 100).toLocaleString() + '% , totalling $ ' + ((data.regularMarketPrice * data.shares) - (data.purchasePrice * data.shares)).toFixed(2)}</Card>
-                        
-                  )) 
-                  : <>
-                    <h2>No assets to refresh</h2>
-                  </>
-              }
-            </ul>
-          </div>
-          <div>
-        
-            {/* <SendEmail />
-            <br />
-            <FormTextExample />
-            <br />
-            <ImageFile images={images}/> */}
-            </div>
-        </div>
+
+    <div   className='cass'>
+    <ul className='happy'>
+      {
+            mergedData && mergedData.length
+              ? mergedData.map(data => (
+
+                <li
+                  key={data?.symbol}
+                  blog={data}>
+                    {data.symbol+ ' ' +
+                    (data.regularMarketPrice* data.shares).toFixed(2)+
+                   '... you paid '+
+                   (data.purchasePrice * data.shares).toFixed(2)+ 
+                  '... a  '+  
+                  (
+                    (
+                      (data.regularMarketPrice* data.shares) - 
+                      (data.purchasePrice* data.shares))/
+                      (data.purchasePrice* data.shares) * 100).toFixed(2)
+                      +'%'}</li>
+              ))
+              : <>
+                <h2>No Blogs Yet... Add one in the Form Above</h2>
+                </>
+        }
+    </ul>
+    </div>
+
+    <div>
+      
+   
+  
+    </div>
+    </div>
       </header>
 
     </div>
   );
 
+  function cardBody (data) {
 
+  }
+
+  function handleDragEnd(event) {
+    console.log("Drag end called");
+    const {active, over} = event;
+    console.log("ACTIVE: " + active.id);
+    console.log("OVER :" + over.id);
+
+    if(active.id !== over.id) {
+      setMergedData((items) => {
+        const activeIndex = items.indexOf(active.id);
+        const overIndex = items.indexOf(over.id);
+        console.log(arrayMove(items, activeIndex, overIndex));
+        return arrayMove(items, activeIndex, overIndex);
+        // items: [2, 3, 1]   0  -> 2
+        // [1, 2, 3] oldIndex: 0 newIndex: 2  -> [2, 3, 1] 
+      });
+      
+    }
+  }
 }
 
 export default App;
+
+
+
+
+
+// import { React, useState, useEffect } from 'react';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import { DndContext, closestCenter } from '@dnd-kit/core';
+// import { arrayMove,SortableContext,
+// verticalListSortingStrategy,
+// } from '@dnd-kit/sortable';
+// import { SortableItem } from './components/SortableItem';
+// import List from './components/List';
+
+// import { Card } from 'react-bootstrap';
+// import './App.css';
+// // import FormTextExample from './components/FormTextExample';
+// // import SendEmail from './components/SendEmail'
+// // import ImageFile from './components/ImageFile';
+
+// import './App.css';
+// const SERVER_URL = "http://localhost:3008/api/portfolio/"
+
+// function App() {
+//   const [mergedData, setMergedData] = useState([])
+//   const [errorMessage, setErrorMessage] = useState("");
+//   // const [images, setImages] = useState([]);
+//   // const maxNumber = 69;
+
+//   // const onChange = (imageList, addUpdateIndex) => {
+//   //   // data for submit
+//   //   console.log(imageList, addUpdateIndex);
+//   //   setImages(imageList);
+//   //   };
+
+//   const getMergedData = async () => {
+//     try {
+//       const response = await fetch(
+//         SERVER_URL
+//       );
+//       const data = await response.json();
+//       setMergedData(data);
+//       console.log(data)
+//     } catch (e) {
+//       console.error(e);
+//       setErrorMessage(e.message);
+//     }
+//   };
+
+//   console.log(mergedData.length)
+//   useEffect(() => { }, []);
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <div>
+//           <button type='submit' onClick={getMergedData}>Click to prompt the Server to get current stock prices</button>
+//         </div>
+//         <div>
+//           <div className='cass'>
+//             <ul className='happy'>
+//               {
+//                 mergedData && mergedData.length
+//                   ? mergedData.map(data => (
+//                     <Card
+//                       key={data?.symbol}
+//                       blog={data}>
+//                         {data.symbol + ' $' + (data.regularMarketPrice * data.shares).toLocaleString() +
+//                         ', you paid $' +
+//                         (data.purchasePrice * data.shares).toLocaleString() +
+//                         ' a  ' + (((data.regularMarketPrice * data.shares) - (data.purchasePrice * data.shares)) / (data.purchasePrice * data.shares) * 100).toLocaleString() + '% , totalling $ ' + ((data.regularMarketPrice * data.shares) - (data.purchasePrice * data.shares)).toFixed(2)}</Card>
+                        
+//                   )) 
+//                   : <>
+//                     <h2>No assets to refresh</h2>
+//                   </>
+//               }
+//             </ul>
+//           </div>
+//           <div>
+        
+//             {/* <SendEmail />
+//             <br />
+//             <FormTextExample />
+//             <br />
+//             <ImageFile images={images}/> */}
+//             </div>
+//         </div>
+//       </header>
+
+//     </div>
+//   );
+
+
+// }
+
+// export default App;
